1afbb25eda854fb3cd982717c98c550eb28b6355
argialia <2129812409@qq.com>
2024-10-20 19:03:18 +0800

    Update .py

--- .py
+++ .py
@@ -1,28 +1,28 @@
 #1.
 def add(a:int,b:int)-> int:
-    return a + b
-a = int(input())
-b = int(input())
+   return a + b
+a = int(input("a:"))
+b = int(input("b:"))
 print("a+b={}",format(add(a+b)))
 #2.
 N = int(input(""))
 if N >=0 and N % 10 != 0:
-    a = str(N)[::-1]
-    print(a)
+   a = str(N)[::-1]
+   print(a)
 elif N < 0 and N % 10 != 0:
-    b = -N
-    c = str(b)[::-1]
-    i = -int(c)
-    print(i)
+   b = -N
+   c = str(b)[::-1]
+   i = -int(c)
+   print(i)
 elif N >= 0 and N % 10 == 0:
-    d = N // 10
-    f = str(d)[::-1]
-    print(f)
+   d = N // 10
+   f = str(d)[::-1]
+   print(f)
 elif N < 0 and N % 10 == 0:
-    g = N // 10
-    h = str(-g)[::-1]
-    j = -int(h)
-    print(j)
+   g = N // 10
+   h = str(-g)[::-1]
+   j = -int(h)
+   print(j)
 #3.
 str = (input(""))
 num_0 = 0
@@ -57,3 +57,57 @@ for i in str:
     elif i == '9':
         num_9 += 1
 print(f'num_0: {num_0}\nnum_1: {num_1}\nnum_2: {num_2}\nnum_3: {num_3}\nnum_4: {num_4}\nnum_5: {num_5}\nnum_6: {num_6}\nnum_7: {num_7}\nnum_8: {num_8}\nnum_9: {num_9}')
+#4.
+# 变量初始化
+total_scholarships = 0
+max_scholarship = 0
+student_with_max_scholarship = None
+students = []
+# 定义计算奖学金的函数
+def calculate_scholarship(student):
+    scholarships = {
+        'academician': 8000,
+        'youth': 4000,
+        'excellent': 2000,
+        'west': 1000,
+        'class_contribution': 850
+    }
+    total = 0
+    if student['average'] > 80 and student['publications'] >= 1:
+        total += scholarships['academician']
+    if student['average'] > 85 and student['class_assessment'] > 80:
+        total += scholarships['youth']
+    if student['average'] > 90:
+        total += scholarships['excellent']
+    if student['average'] > 85 and student['west_province']:
+        total += scholarships['west']
+    if student['class_assessment'] > 80 and student['class_leader']:
+        total += scholarships['class_contribution']
+    return total
+N = int(input("学生总数：\n"))
+for _ in range(N):
+    student_input = input().split()
+    name = student_input[0]
+    average = int(student_input[1])
+    class_assessment = int(student_input[2])
+    class_leader = student_input[3] == 'Y'
+    west_province = student_input[4] == 'Y'
+    publications = int(student_input[5])
+
+    student_data = {
+        'name': name,
+        'average': average,
+        'class_assessment': class_assessment,
+        'class_leader': class_leader,
+        'west_province': west_province,
+        'publications': publications
+    }
+    students.append(student_data)
+    scholarship = calculate_scholarship(student_data)
+    total_scholarships += scholarship
+    if scholarship > max_scholarship:
+        max_scholarship = scholarship
+        student_with_max_scholarship = name
+print(student_with_max_scholarship)
+print(max_scholarship)
+print(total_scholarships)
